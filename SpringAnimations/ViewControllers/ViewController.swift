57d6f10e594fa2e65055a71b7c49e2892c52f842
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-03-12 18:16:14 +0300

    make animation VC

--- SpringAnimations/ViewControllers/ViewController.swift
+++ SpringAnimations/ViewControllers/ViewController.swift
@@ -6,21 +6,54 @@
 //
 
 import UIKit
+import SpringAnimation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet var delayLabel: UILabel!
     @IBOutlet var durationLabel: UILabel!
     @IBOutlet var forceLabel: UILabel!
     @IBOutlet var curveLabel: UILabel!
     @IBOutlet var presetLabel: UILabel!
-    @IBOutlet var animatedView: UIView!
+    
+    @IBOutlet var animatedView: SpringView!
     @IBOutlet var runButton: UIButton!
+    
+    private var nextAnimation = Animation.getAnimation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        animatedView.layer.cornerRadius = 20
+        runButton.layer.cornerRadius = 10
+        
+        setLabelsFor(nextAnimation)
     }
 
-
+    @IBAction func startSpringAnimation(_ sender: SpringButton) {
+        let animation = nextAnimation
+        nextAnimation = Animation.getAnimation()
+        
+        sender.setTitle("Run \(nextAnimation.preset)", for: .normal)
+        
+        setLabelsFor(animation)
+        
+        animatedView.animation = animation.preset
+        animatedView.curve = animation.curve
+        animatedView.force = animation.force
+        animatedView.duration = animation.duration
+        animatedView.delay = animation.delay
+        
+        animatedView.animate()
+    }
+    
+    private func setLabelsFor(_ animation: Animation) {
+        presetLabel.text = "preset: \(animation.preset)"
+        curveLabel.text = "curve: \(animation.curve)"
+        forceLabel.text = String(format: "force: %.2f", animation.force)
+        durationLabel.text = String(format: "duration: %.2f", animation.duration)
+        delayLabel.text = String(format: "delay: %.2f", animation.delay)
+    }
+    
 }
 
